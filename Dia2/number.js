d55ff6b1701c14323747cae96b016c93bc9d72fb
Marcelo Reyna Rangel <marcelo@192.168.0.21>
2026-03-06 14:30:40 -0600

    add line

---
+++ Dia2/number.js
@@ -0,0 +1,15 @@
+//Number (Número)
+let entero = 9;
+let decimal = 8.7;
+let infintio = Infinity;
+let inderteminado = NaN; // "not at Number"
+
+//Operaciones 
+console.log(7 / 2); // 3.5..
+console.log(0 / 0);  //NaN
+console.log(1 / 0); //Infinity
+
+//verificar si es numero valido
+console.log(isNaN(NaN));  //true
+console.log(isFinite(10)); //true
+console.log(Number.isInteger(5)); //true
