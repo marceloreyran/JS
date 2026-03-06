9176479926831c0a7578620bd663d96c7078a879
Marcelo Reyna Rangel <marcelo@192.168.0.21>
2026-03-06 12:51:31 -0600

    add commit

--- Dia2/Eje1.js
+++ Dia2/Eje1.js
@@ -3,3 +3,10 @@ let edad = 39;
 let ciudad = "Monterrey";
 let pais = "Mexico";
 console.log(nombre,edad,ciudad,pais);
+
+let profesion = "Ingeniero";
+let salario = "30,000";
+let empresa = "Tresande";
+let area = "IT";
+console.log(profesion,salario,empresa,area);
+
