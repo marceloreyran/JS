9026d3438c18112b980be07a94bddb8e873fd274
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-09 11:18:40 -0600

    commit conversion

---
+++ Dia2/conver.js
@@ -0,0 +1,24 @@
+//string a number
+Number("123"); //123
+parseInt("123px");
++ "123";
+console.log(Number("123")); //123
+console.log(parseInt("123px")); //123
+console.log(+"123"); //123
+
+// Number a String
+String(42);        // "42"
+(42).toString();   // "42"
+42 + "";           // "42"
+
+//Boolean 
+Boolean(1);
+Boolean(0);
+!!1;
+console.log(Boolean(1));
+
+// conversion atutomatica
+console.log("5" + "3"); // "53" (string concatenation)
+console.log("10" - "4"); // 6 (subtraction, converts to numbers)
+console.log("10" * "20");  // 200 (multiplication, converts to numbers)
+console.log("10" / "2");   // 5 (division, converts to numbers)
