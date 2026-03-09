88cd6004dff9b5fb99201d2db3e5e147c00490e9
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-09 11:51:41 -0600

    add table

--- Dia2/conver.js
+++ Dia2/conver.js
@@ -22,3 +22,8 @@ console.log("5" + "3"); // "53" (string concatenation)
 console.log("10" - "4"); // 6 (subtraction, converts to numbers)
 console.log("10" * "20");  // 200 (multiplication, converts to numbers)
 console.log("10" / "2");   // 5 (division, converts to numbers)
+console.log(10 + 10);      // 20
+console.log(5 + "10");  // "510" (number + string results in string concatenation)
+console.log("5" - 2);    // 3 (string converted to number)
+console.log("5" * 2);    // 10 (string converted to number)
+console.log("5" / 2);    // 2.5 (string converted to number)
---
+++ Dia2/table.js
@@ -0,0 +1,12 @@
+//Resumen en la Tabla 
+/** 
+ | Tipo      | Ejemplo         | Características                 |
+| --------- | --------------- | ------------------------------- |
+| String    | `"hola"`        | Texto, inmutable                |
+| Number    | `42`, `3.14`    | Números, incluye NaN e Infinity |
+| Boolean   | `true`, `false` | Solo dos valores                |
+| Undefined | `undefined`     | Sin valor asignado              |
+| Null      | `null`          | Vacío intencional               |
+| BigInt    | `42n`           | Números muy grandes             |
+| Symbol    | `Symbol("id")`  | Valores únicos                  |
+*/
