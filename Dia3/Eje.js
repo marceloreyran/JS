38476965cd365c05d8708b15c3b9dcc82877a578
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-03-10 17:32:16 -0600

    Add 'rin' property to auto object

--- Dia3/Eje.js
+++ Dia3/Eje.js
@@ -3,9 +3,11 @@ const auto = {
     modelo : "cx-50",
     año: 2026,
     precio: 150000,
+    rin : 18
 };
 
 console.log(auto.marca);
 console.log(auto.modelo);
 console.log(auto.año);
+console.log(auto.precio);
 console.log(auto.precio);
