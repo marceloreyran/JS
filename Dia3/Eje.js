44173012fc35d068f6357e8d88bb13554fbb752e
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-10 15:22:19 -0600

    line eje

---
+++ Dia3/Eje.js
@@ -0,0 +1,11 @@
+const auto = {
+    marca : "mazda",
+    modelo : "cx-50",
+    año: 2026,
+    precio: 150000,
+};
+
+console.log(auto.marca);
+console.log(auto.modelo);
+console.log(auto.año);
+console.log(auto.precio);
