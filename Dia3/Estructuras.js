ac8e6672bda5d8dd9047d24d0105fdcf4f2eb266
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-10 14:10:37 -0600

    add folder 3

--- Dia3/Estructuras.js
+++ Dia3/Estructuras.js
@@ -1,11 +1,23 @@
 //Estructuras de Datos Básicas en JavaScript
 /**
  * Primitivios
- * string: "hola", 'mundo', `plantilla`
- * number: 42, 3.14, -1, 4.14, infiniti, -infiniti, NaN
- * boolean: true, false
- * null: null
- * undefined: undefined
- * symbol: Symbol('descripcion')
- * bigint: 9007199254740991n
+ * String: "hola", 'mundo', `plantilla`
+ * Number: 42, 3.14, -1, 4.14, infiniti, -infiniti, NaN
+ * Boolean: true, false
+ * Null: null
+ * Undefined: undefined
+ * Symbol: Symbol('descripcion')
+ * Bigint: 9007199254740991n
  */
+
+//Objetos
+ const persona ={
+    nombre : "Marcelo",
+    edad : 30,
+    trabajo: "Desarrollador",
+    municipio : "San Pedro",
+    saludar(){console.log("Hola soy  " + this.nombre); }
+    };
+
+    console.log(persona.nombre);
+    persona.saludar();
