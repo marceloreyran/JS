df8aa1f56ea203f29cfbeb196024246657b4e340
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-11 11:40:23 -0600

    add line folder 3

---
+++ Dia3/arrays.js
@@ -0,0 +1,9 @@
+// Arrays
+
+const number =[1, 2, 3, 4, ,5];
+const mixed = [1, "two", true,{clave: "valor"}];
+console.log(number[0]);
+console.log(mixed[1]);
+number.push(6);
+console.log(number);
+number.pop();
