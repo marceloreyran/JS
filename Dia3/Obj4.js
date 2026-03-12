7a82ca15c97650ccb52fd5c345c3e608cc9db308
Marcelo Reyna Rangel <marcelo@192.168.0.20>
2026-03-12 16:18:18 -0600

    add Eje3 commit

---
+++ Dia3/Obj4.js
@@ -0,0 +1,40 @@
+//Ejercicio 4: Objeto Banco (Más Complejo)
+
+//Crea un objeto que simule una cuenta bancaria con operaciones.
+
+const cuenta = {
+    titular: "Marcelo",
+    saldo: 10000,
+    historial: ["Ene","Feb","Marz"],
+
+    depositar: function(cantidad){
+        this.saldo += cantidad;
+        this.historial.push(`Depositar: +${cantidad}`);
+        return `Deposito de ${cantidad} pesos fue realizado, el saldo actual es: ${this.saldo}`
+    },
+
+    retirar: function(cantidad){
+        if (cantidad > this.saldo){
+            return "saldo insuficiente";
+        }
+        this.saldo -= cantidad;
+        this.historial.push(`Retirar: -${cantidad}`);
+        return `El retiro de ${cantidad} pesos fue realizado, el saldo actual es: ${this.saldo}`
+    },
+    verSaldo: function(cantidad){
+        return `El saldo disponible es de $${this.saldo} pesos`;
+    },
+
+    verHistorial: function(){
+        return `Este es el historial de transacciones: ${this.historial.join(", ")}`;
+
+    }
+};
+
+
+
+
+console.log(cuenta.depositar(1000));
+console.log(cuenta.retirar(5100));
+console.log(cuenta.verSaldo());
+console.log(cuenta.verHistorial());
