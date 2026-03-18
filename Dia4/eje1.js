cd26bb840f6d30719a84408085e22e56c09933d8
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-03-18 14:32:05 -0600

    first commit line

---
+++ Dia4/eje1.js
@@ -0,0 +1,16 @@
+// Ejercicio 1: Objeto Persona con Propiedades Anidadas
+const persona = {
+    nombre: "Marcelo",
+    edad: 30,
+    direccion: {
+        calle: "Calle Falsa 123",
+        ciudad: "Ciudad Ejemplo",
+        pais: "País Ejemplo"
+    },
+    mostrarInfo: function() {
+        return `Nombre: ${this.nombre}, Edad: ${this.edad}`;
+    }
+};
+
+console.log(persona.mostrarInfo());
+console.log(`Dirección: ${persona.direccion.calle}, ${persona.direccion.ciudad}, ${persona.direccion.pais}`);
