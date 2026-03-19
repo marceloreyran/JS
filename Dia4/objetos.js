e6bf804c2ea7c2cb0366e85ba846c9dbb719929d
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-03-19 16:20:16 -0600

    add new line

---
+++ Dia4/objetos.js
@@ -0,0 +1,12 @@
+//Este es un objeto basico 
+
+const persona = {
+    nombre: "mario",
+    apellido: "Rangel",
+    edad:30,
+    ciudad: "CDMX"
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.ciudad);
