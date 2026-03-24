146265a7eabe263ad67ae30307cca12c53b04ec4
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-03-24 12:40:49 -0600

    add array file

---
+++ Dia5/array.js
@@ -0,0 +1,15 @@
+//Array []
+
+/** 
+ * > ¿Para qué sirve? Para guardar listas ordenadas de cosas. Como una fila de cajones numerados.
+
+Caso real: Lista de productos en un carrito de compras.
+ */
+
+
+
+
+//Crear un array vacío
+let carrito = ["agua", "pan", "leche"];
+console.log(carrito[0]);
+console.log(carrito.length);
