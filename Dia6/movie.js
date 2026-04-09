79b242c19be1b01d9827b98fa796ae46ef1b8033
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-04-09 11:42:51 -0600

    other commit line

--- Dia6/movie.js
+++ Dia6/movie.js
@@ -5,8 +5,12 @@ const movie = {
     actor: "Marcelo",
     año: 2026,
     genero: "Documental"
+     
 };
 
 console.log(movie.titulo);
 console.log(movie.actor);
 console.log("Año del lansamiento:"+ movie.año);
+console.log("Genero de la movie:" + movie.genero);
+console.log("Año de lanzamiento: " + movie.año);
+console.log(movie.obtenerResumen());
