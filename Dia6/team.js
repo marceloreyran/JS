2a51c51a9fc68dc4d1eea545b5dcccb8d3c8b3b2
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2026-04-10 15:47:18 -0600

    add line team

--- Dia6/team.js
+++ Dia6/team.js
@@ -2,7 +2,7 @@ const team = {
     name: "Rayados", 
     city: "Monterrey",
     number: 11,
-    players: ["mochis", "Chavez","Guzman","Salcedo", "Arteaga", "Rodriguez", "Torres","Fimbrez","Corona","Canales","Juca"],
+    players: ["Mochis", "Chavez","Guzman","Salcedo", "Arteaga", "Rodriguez", "Torres","Fimbrez","Corona","Canales","Juca"],
     position: 8,
     getPlayer: function() {
         return this.players[this.position + 2];   
@@ -15,3 +15,5 @@ const team = {
 console.log(team.getPlayer());
 console.log(team.name);
 console.log(team.city);
+console.log(team.number);
+console.log(team.players);
